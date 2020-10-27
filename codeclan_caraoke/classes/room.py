afe00efed1b020e7d0e437bd9897a72200bd7a22
Marc Downie <marcdownie3@hotmail.com>
2020-10-27 14:53:47 +0000

    fixed my errors, only managed minimum due to time

--- codeclan_caraoke/classes/room.py
+++ codeclan_caraoke/classes/room.py
@@ -1,9 +1,18 @@
 class Room:
     def __init__(self, room_name):
         self.room_name = room_name
-        self.size = {}
+        self.size = 0
         self.guests = []
         self.playlist = []
 
-    # def add_guest_in_room(self, guest):
-    #     self.guests.append(guest)
+    def add_guest_in_room(self, guest):
+        self.guests.append(guest)
+
+    def remove_guest_from_room(self, guest):
+        self.guests.remove(guest)
+        
+    def add_song_to_room(self, song):
+        self.playlist.append(song)
+
+    def remove_song_from_room(self, song):
+        self.playlist.remove(song)
--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -12,15 +12,24 @@ class TestRoom(unittest.TestCase):
         self.guest_2 = Guest("Marc", "Breath in space", 50)
         self.guest_3 = Guest("Linzi", "Around the world", 25)
         self.guest_4 = Guest("Becca", "Take on me", 15)
+        self.song = Song("Enter Shikari", "Breath in space")
 
         # rooms = [self.room_1, self.room_2]
 
-    # def test_room_add_guests(self):
-    #     self.room.add_guest_in_room(self.guest_1)
-    #     self.assertEqual(1, len(self.room.guests))
+    def test_room_add_guests(self):
+        self.room_1.add_guest_in_room(self.guest_1)
+        self.assertEqual(1, len(self.room_1.guests))
 
-    # def test_remove_guests(self):
-    #     self.assertEqual(0, self.room_1.size)
+    def test_remove_guests(self):
+        self.room_1.add_guest_in_room(self.guest_1)
+        self.room_1.remove_guest_from_room(self.guest_1)
+        self.assertEqual([], self.room_1.guests)
 
-    def test_add_song_to_room__empty(self):
-        self.assertEqual(0, len(self.room.playlist))
+    def test_adding_song_to_room(self):
+        self.room_1.add_song_to_room(self.song)
+        self.assertEqual(1, len(self.room_1.playlist))
+
+    def test_removing_song_from_room(self):
+        self.room_1.add_song_to_room(self.song)
+        self.room_1.remove_song_from_room(self.song)
+        self.assertEqual([], self.room_1.playlist)
